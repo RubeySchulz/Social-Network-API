f71bca6b16b79be6f0e79a42310f06f75e931681
Ruben <[connor.j.schulz@gmail.com]>
2022-07-19 17:38:30 -0400

    made user routes

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+const UserSchema = new Schema (
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: [validateEmail, 'Please fill a valid email']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
